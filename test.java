345d485086b973cf113e56bac1600f2cb7a2c87d
github20032003 <209X1A05H6@GPREC.AC.IN>
2022-06-29 22:23:30 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,10 @@ public class test
 {
     public static void main(String [] args)
     {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(n);
         System.out.println("Hellow");
+
     }
 }
